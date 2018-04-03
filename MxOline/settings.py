c8a2ad9929e7971976a7bdf32a7a0a3203404547
k-kyle <405942850@qq.com>
2018-04-03 13:54:09 +0800

    修复userprofile表性别字段长度过段的问题。和设置为中文、本地时区。

--- MxOline/settings.py
+++ MxOline/settings.py
@@ -116,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Hans'
 
 TIME_ZONE = 'UTC'
 
@@ -124,7 +124,7 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
